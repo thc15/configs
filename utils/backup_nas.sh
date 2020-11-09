d098bc5fd1b1f3d1aadf409d38446c4ba56c56ed
thc15 <thcostis@gmail.com>
2020-11-09 10:54:37 +0100

    2020-11-09 10:54:37

--- utils/backup_nas.sh
+++ utils/backup_nas.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+#!/bin/bash -x
 
 USER="pi"
 NAS_IP="192.168.1.25"
 RSYNC="rsync -atvuE --inplace --progress"
+MOUNT_PT="/media/pi"
 
-ssh $USER@$NAS_IP "ls /media/pi" | while read hdd; do
-        $RSYNC $HOME/Pictures/ $USER@$NAS_IP:/$hdd/photos/
-        $RSYNC $HOME/Documents/ $USER@$NAS_IP:/$hdd/documents/
-        $RSYNC $HOME/work/ $USER@$NAS_IP:/$hdd/work/
+ssh $USER@$NAS_IP "ls $MOUNT_PT" | while read hdd; do
+        $RSYNC $HOME/Pictures/ $USER@$NAS_IP:$MOUNT_PT/$hdd/photos/
+        $RSYNC $HOME/Documents/ $USER@$NAS_IP:$MOUNT_PT/$hdd/documents/
+        $RSYNC $HOME/work/ $USER@$NAS_IP:$MOUNT_PT/$hdd/work/
 done
